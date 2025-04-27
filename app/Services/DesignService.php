f2daf88e9ef98d93fa3c88e43b290fffd3e8c60d
Ghaith-ElHamwi <ghaithhamwi123@outlook.com>
2025-04-27 11:40:25 +0300

    fiX the design search

--- app/Services/DesignService.php
+++ app/Services/DesignService.php
@@ -13,7 +13,7 @@ class DesignService
         $dto->designs = Design::with(['categories', 'likers'])
             ->when($dto->hasCategoryFilter(), fn($q) => $q->whereRelation('categories', 'categories.id', $dto->category_id))
             ->when($dto->hasPrintTypeFilter(), fn(Builder $q) => $q->where('print_type', $dto->print_type))
-            ->when($dto->hasSearchFilter(), fn(Builder $q) => $q->where('title', 'LIKE', $dto->query))
+            ->when($dto->hasSearchFilter(), fn(Builder $q) => $q->where('title', 'LIKE', "%$dto->query%"))
             ->when($dto->hasFreeFilter(), fn(Builder $q) => $q->where('price', 0))
             ->when($dto->hasLikedFilter(), fn(Builder $q) => $q->isLiker(auth()->user()))
             ->paginate();
